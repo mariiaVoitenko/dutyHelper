8097f2e6ed2b7155566b658506e902ff74abe529
mariiaVoitenko <mariya.voytenko@gmail.com>
2015-05-28 22:28:59 +0300

    Category table

---
+++ src/main/webapp/scripts/app/entities/category/category.controller.js
@@ -0,0 +1,74 @@
+'use strict';
+
+angular.module('dutyhelperApp')
+    .controller('CategoryController', function ($scope, Category, ParseLinks) {
+        $scope.categorys = [];
+        $scope.page = 1;
+        $scope.loadAll = function() {
+            Category.query({page: $scope.page, per_page: 20}, function(result, headers) {
+                $scope.links = ParseLinks.parse(headers('link'));
+                for (var i = 0; i < result.length; i++) {
+                    $scope.categorys.push(result[i]);
+                }
+            });
+        };
+        $scope.reset = function() {
+            $scope.page = 1;
+            $scope.categorys = [];
+            $scope.loadAll();
+        };
+        $scope.loadPage = function(page) {
+            $scope.page = page;
+            $scope.loadAll();
+        };
+        $scope.loadAll();
+
+        $scope.showUpdate = function (id) {
+            Category.get({id: id}, function(result) {
+                $scope.category = result;
+                $('#saveCategoryModal').modal('show');
+            });
+        };
+
+        $scope.save = function () {
+            if ($scope.category.id != null) {
+                Category.update($scope.category,
+                    function () {
+                        $scope.refresh();
+                    });
+            } else {
+                Category.save($scope.category,
+                    function () {
+                        $scope.refresh();
+                    });
+            }
+        };
+
+        $scope.delete = function (id) {
+            Category.get({id: id}, function(result) {
+                $scope.category = result;
+                $('#deleteCategoryConfirmation').modal('show');
+            });
+        };
+
+        $scope.confirmDelete = function (id) {
+            Category.delete({id: id},
+                function () {
+                    $scope.reset();
+                    $('#deleteCategoryConfirmation').modal('hide');
+                    $scope.clear();
+                });
+        };
+
+        $scope.refresh = function () {
+            $scope.reset();
+            $('#saveCategoryModal').modal('hide');
+            $scope.clear();
+        };
+
+        $scope.clear = function () {
+            $scope.category = {name: null, id: null};
+            $scope.editForm.$setPristine();
+            $scope.editForm.$setUntouched();
+        };
+    });
