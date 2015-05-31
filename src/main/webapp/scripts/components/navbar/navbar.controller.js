eec52fa7c88e655abc5b551d948258ee8bcae94a
mariiaVoitenko <mariya.voytenko@gmail.com>
2015-05-31 20:04:38 +0300

    users' navbar html

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -3,8 +3,6 @@
 angular.module('dutyhelperApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal) {
         $scope.isAuthenticated = Principal.isAuthenticated;
-        //TODO
-        //$scope.isAdmin=Principal.isInRole()
         $scope.$state = $state;
 
         $scope.logout = function () {
